34383afc882cd5ecc2e7a3a5796b4487d2fdeecc
BIRSAx2 <Mouhieddinesabir@gmail.com>
2020-11-19 19:45:20 +0100

    Added Publisher and PublisherRepository. Defined the relation between Publisher and Book (OneToMany). Tested everything using the BootStrapData class.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,33 +14,54 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository= publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        System.out.println("Started in BootStrap");
+
+        Publisher feltrinelliEditore = new Publisher();
+        feltrinelliEditore.setName("Feltrinelli Editore");
+        feltrinelliEditore.setCity("Milan");
+        feltrinelliEditore.setState("Italy");
+
+        publisherRepository.save(feltrinelliEditore);
+
+
         Author harukiMurakami = new Author("Haruki","Murakami");
         Book norwegianWood = new Book("Norwegian Wood","1234567890");
 
         harukiMurakami.getBooks().add(norwegianWood);
         norwegianWood.getAuthors().add(harukiMurakami);
+        norwegianWood.setPublisher(feltrinelliEditore);
+        feltrinelliEditore.getBooks().add(norwegianWood);
+
+        authorRepository.save(harukiMurakami);
+        bookRepository.save(norwegianWood);
+        publisherRepository.save(feltrinelliEditore);
+
 
         Author kenFollet = new Author("Ken","Follet");
         Book fallOfGiants = new Book("Fall Of Giants","0987654321");
 
         kenFollet.getBooks().add(fallOfGiants);
         fallOfGiants.getAuthors().add(kenFollet);
+        fallOfGiants.setPublisher(feltrinelliEditore);
+        feltrinelliEditore.getBooks().add(fallOfGiants);
 
-
-        authorRepository.save(harukiMurakami);
         authorRepository.save(kenFollet);
-        bookRepository.save(norwegianWood);
         bookRepository.save(fallOfGiants);
+        publisherRepository.save(feltrinelliEditore);
 
-        System.out.println("Started in BootStrap");
+        System.out.println("Number of Publisher: "+publisherRepository.count());
         System.out.println("Number of Books: "+bookRepository.count());
+        System.out.println("Publisher Number of Books: "+feltrinelliEditore.getBooks().size());
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -17,9 +17,14 @@ public class Book {
     @JoinTable(
             name = "author_book",
             joinColumns = @JoinColumn(name = "book_id"),
-            inverseJoinColumns = @JoinColumn(name="author_id")
+            inverseJoinColumns = @JoinColumn(name = "author_id")
     )
     private Set<Author> authors = new HashSet<>();
+
+    @ManyToOne
+    private Publisher publisher;
+
+
 
     public Book() {
     }
@@ -84,5 +89,12 @@ public class Book {
                 ", isbn='" + isbn + '\'' +
                 ", authors=" + authors +
                 '}';
+    }
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 }
